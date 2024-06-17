d9c48da4a0d8e5593be04e06032f26c8679e7935
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-17 16:58:12 +0200

    Fix: schema needs to use animal names
    * Since user selects a particular animal on the frontend, the backend
    needs to check coordinates for that particular animal as well

--- phototag-backend/models/animalCoordinates.js
+++ phototag-backend/models/animalCoordinates.js
@@ -2,24 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CoordinatesSchema = new Schema({
-  coords: [
-    {
-      x: [Number, Number],
-      y: [Number, Number],
-    },
-    { x: [Number, Number], y: [Number, Number] },
-    { x: [Number, Number], y: [Number, Number] },
-  ],
+  brownChicken: {
+    x: [Number, Number],
+    y: [Number, Number],
+  },
+  rabbit: { x: [Number, Number], y: [Number, Number] },
+  whiteChicken: { x: [Number, Number], y: [Number, Number] },
 });
 
 const CoordinatesModel = mongoose.model("Coordinates", CoordinatesSchema);
 
 const coordinates = new CoordinatesModel({
-  coords: [
-    { x: [104, 131], y: [153, 168] },
-    { x: [235, 311], y: [127, 152] },
-    { x: [424, 496], y: [225, 259] },
-  ],
+  brownChicken: { x: [104, 131], y: [153, 168] },
+  rabbit: { x: [235, 311], y: [127, 152] },
+  whiteChicken: { x: [424, 496], y: [225, 259] },
 });
 
 // coordinates.save();
