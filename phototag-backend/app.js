7caa67a7e591daead024cbb39baf6b868d3866ff
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-17 16:59:54 +0200

    Feat: allow all CORS for development in frontend

--- phototag-backend/app.js
+++ phototag-backend/app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const indexRouter = require("./routes/index");
 require("./config/database");
+const cors = require("cors");
 const app = express();
 
 // view engine setup
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 
