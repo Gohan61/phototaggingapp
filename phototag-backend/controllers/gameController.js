e1e2557a3e5b45a49868515791301e3a492a9789
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-17 17:01:11 +0200

    Refactor: check coordinates in line w/ new Schema

--- phototag-backend/controllers/gameController.js
+++ phototag-backend/controllers/gameController.js
@@ -3,19 +3,19 @@ const Coordinates = require("../models/animalCoordinates");
 
 exports.getCoordinates = asyncHandler(async (req, res, next) => {
   const coordinates = await Coordinates.findById(
-    "6670343dc291fd6c5d232b3e"
+    "66704aee26485e9e9147fcea"
   ).exec();
 
-  for (const coordinate of coordinates.coords) {
-    console.log(coordinate);
-    if (
-      coordinate.x[0] <= req.body.x &&
-      req.body.x <= coordinate.x[1] &&
-      coordinate.y[0] <= req.body.y &&
-      req.body.y <= coordinate.y[1]
-    ) {
-      return res.status(200).json({ message: "Coordinates in range" });
-    }
+  const animal = coordinates[req.body.animal];
+
+  if (
+    animal.x[0] <= req.body.x &&
+    req.body.x <= animal.x[1] &&
+    animal.y[0] <= req.body.y &&
+    req.body.y <= animal.y[1]
+  ) {
+    return res.status(200).json({ message: "Coordinates in range" });
   }
+
   return res.status(200).json({ message: "Coordinates not in range" });
 });
